1ef1507deb2593a83b0a87eb1b7b90de062a5f62
NEO <nainglinaung100kta@gmail.com>
2025-02-25 17:53:58 +0630

    Feb 25 2025

--- python_basics/files_exceptions.py
+++ python_basics/files_exceptions.py
@@ -162,3 +162,270 @@
 #     all_guests += f"{guest}\n"
     
 # path.write_text(all_guests)
+
+# -----------------------------------------------------------------------------
+
+
+# Exception
+
+# print(5/0) --> error traceback
+
+# Using try-except Blocks 
+
+# try:
+#     print(5/0)
+# except ZeroDivisionError:
+#     print("You can't divide by zero!")
+
+# Using Exception to Prevent Crashes
+
+# print("Give me two numbers, and I'll divide them.")
+# print("Enter 'q' to quit.")
+
+# while True:
+#     first_num = input("\nFirst number: ")
+#     if first_num == 'q':
+#         break
+
+#     second_num = input("Second number: ")
+#     if second_num == 'q':
+#         break
+
+    # answer = int(first_num) / int(second_num) --> if second num is 0, it's error 
+    # print(answer) 
+
+# print("Give me two numbers, and I'll divide them.")
+# print("Enter 'q' to quit.")
+
+# while True:
+#     first_num = input("\nFirst number: ")
+#     if first_num == 'q':
+#         break
+
+#     second_num = input("Second number: ")
+#     if second_num == 'q':
+#         break
+
+#     try:
+#         answer = int(first_num) / int(second_num)
+#     except ZeroDivisionError:
+#         print("You can't divide by zero!")
+#     else:
+#         print(answer)
+
+# -----------------------------------------------------------------------------
+
+# Handling the FileNotFoundError Exception
+
+# from pathlib import Path
+
+# path = Path('alice.txt')
+# content = path.read_text(encoding='utf-8')
+
+
+# from pathlib import Path 
+
+# path = Path('alice.txt')
+# try:
+#     contents = path.read_text(encoding='utf-8')
+# except FileNotFoundError:
+#     print(f"Sorry, the file {path} does not exist.")
+
+
+
+# from pathlib import Path 
+
+# path = Path('alice.txt')
+# try:
+#     contents = path.read_text(encoding='utf-8')
+# except FileNotFoundError:
+#     print(f"Sorry, the file {path} doesn't exist.")
+# else:
+#     words = contents.split()
+#     num_words = len(words)
+#     print(f"The file {path} has about {num_words} words.")
+
+
+# -----------------------------------------------------------------------------
+
+# Working with Multiple Files 
+
+# from pathlib import Path 
+
+# def count_words(path):
+#     try:
+#         contents = path.read_text(encoding='utf=8')
+#     except FileNotFoundError:
+#         print(f"The file {path} doens't exist.")
+#     else:
+#         num_words = len(contents.split())
+#         print(f"The file {path} has {num_words} words.")
+
+# # path = Path("alice.txt")
+# # count_words(path)
+
+# file_names = ['alice.txt', 'siddhartha.txt', 'moby_dick.txt',
+#               'little_woman.txt']
+
+# for file_name in file_names:
+#     path = Path(file_name)
+#     count_words(path)
+
+# -----------------------------------------------------------------------------
+
+# Failing Silently
+
+# from pathlib import Path 
+
+# def count_words(path):
+#     try:
+#         contents = path.read_text(encoding='utf=8')
+#     except FileNotFoundError:
+#         pass
+#     else:
+#         num_words = len(contents.split())
+#         print(f"The file {path} has {num_words} words.")
+
+# # path = Path("alice.txt")
+# # count_words(path)
+
+# file_names = ['alice.txt', 'siddhartha.txt', 'moby_dick.txt',
+#               'little_woman.txt']
+
+# for file_name in file_names:
+#     path = Path(file_name)
+#     count_words(path)
+
+# Deciding Which Errors to Report
+
+# You have to decide which Errors to let the user know and you have to be aware 
+# that some kind of internal information can be in the message to user 
+
+# -----------------------------------------------------------------------------
+
+# Exercises 
+
+# Addition 
+
+# print("Give me two numbers, I'll give you the addition.")
+# print("Enter 'q' to quit.")
+
+# while True:
+#     first_num = input("\nFirst number: ")
+#     if first_num == 'q':
+#         break
+
+#     second_num = input("Second number: ")
+#     if second_num == 'q':
+#         break
+
+#     try:
+#         addition = int(first_num) + int(second_num)
+#     except ValueError:
+#         print("You can't add the text!")
+#     else:
+#         print(addition)
+
+    
+# -----------------------------------------------------------------------------
+
+# Addition Calculator 
+
+# use while loop 
+
+# -----------------------------------------------------------------------------
+
+# Cats & Dogs 
+
+# from pathlib import Path 
+
+# def content(path):
+#     try:
+#         contents = path.read_text(encoding='utf-8').splitlines()
+#     except FileNotFoundError:
+#         print(f"The file {path} doesn't exists.")
+#     else:
+#         print(contents)
+
+# file_names = ['cats.txt', 'dogs.txt']
+# for file_name in file_names:
+#     path = Path(file_name)
+#     content(path)
+
+# -----------------------------------------------------------------------------
+
+# Common Words 
+
+# from pathlib import Path 
+
+# path = Path('programming.txt')
+
+# contents = path.read_text(encoding='utf-8').rstrip()
+# love = contents.lower().count('love ')
+# print(love)
+
+# -----------------------------------------------------------------------------
+
+# Storing Data 
+
+# json.loads() & json.dumps()
+
+
+# from pathlib import Path 
+# import json
+
+# numbers = [2, 3, 5, 7, 11, 13]
+
+# path = Path('numbers.json')
+# contents = json.dumps(numbers)
+# path.write_text(contents)
+
+
+# from pathlib import Path 
+# import json 
+
+# path = Path('numbers.json')
+# contents = path.read_text()
+# numbers = json.loads(contents)
+# print(numbers)
+
+# -----------------------------------------------------------------------------
+
+# Saving and Reading User-Generated Data 
+
+# from pathlib import Path 
+# import json 
+
+# username = input("What is your name? ")
+
+# path = Path('username.json')
+# contents = json.dumps(username)
+# path.write_text(contents)
+  
+# print(f"We'll remember you when you come back, {username}!")
+
+
+# from pathlib import Path 
+# import json 
+
+# path = Path('username.json')
+# contents = path.read_text()
+# username = json.loads(contents)
+# print(f"Welcome back, {username}!")
+
+
+
+from pathlib import Path
+import json 
+
+path = Path('username.json')
+if path.exists():
+    contents = path.read_text()
+    username = json.loads(contents)
+    print(f"Welcome back, {username}!")
+else:
+    username = input("What is your name? ")
+    contents = json.dumps(username)
+    path.write_text(contents)
+
+    print(f"We'll remember you when you come back, {username}!")
