fafdd3f0e6cfc02f15cd4e45950a440b497d55b7
NEO <nainglinaung100kta@gmail.com>
2025-04-12 22:36:38 +0630

    modified Sat Apr 12 10:36 PM 2025

--- python_basics/working_with_lists.py
+++ python_basics/working_with_lists.py
@@ -124,3 +124,185 @@ even_numbers = list(range(2,11,2))
 #     squares.append(value ** 2)
 
 # print(squares)
+
+
+# Simple Statistics with a List of Numbers 
+
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+# print(min(digits))
+
+# print(max(digits))
+
+# print(sum(digits))
+
+
+# List Comprehenisons 
+
+# squares = [value**2 for value in range(1,11)]
+# print(squares)
+
+# -----------------------------------------------------------------------------
+
+# Exercises 
+
+# 4.3 Counting to Twenty 
+
+# for number in range(1,21):
+#     print(number)
+
+
+# 4.4 One Million
+
+# for number in range(1, 1000_001):
+#     print(number)
+
+
+# 4.5 Suming a Million 
+
+# number = [num for num in range(1, 1000_001)]
+# print(sum(number))
+
+
+# 4.6 Odd Numbers 
+
+# for number in range(1, 21, 2):
+#     print(number)
+
+
+# 4.7 Threes
+
+# numbers = [num for num in range(3, 31, 3)]
+# for number in numbers:
+#     print(number)
+
+
+# 4.8 Cubes 
+
+# cubes = []
+# for cube in range(1,11):
+#     cubes.append(cube**3)
+
+# # print(cubes)
+
+# for cube in cubes:
+#     print(cube)
+
+
+# 4.9 Cube Conprehension
+
+# cubes = [cube**3 for cube in range(1,11)]
+# for cube in cubes:
+#     print(cube)
+
+# -----------------------------------------------------------------------------
+
+# Working with Part of a List 
+
+# Slicing a List 
+
+players = ['no', 'yes', 'carl', 'harry', 'leo']
+# print(players[0:3])
+
+# print(players[1:4])
+
+# print(players[:4])
+
+# print(players[2:])
+
+# print(players[-3:])
+
+# Looping Through a Slice 
+
+players = ['no', 'yes', 'carl', 'harry', 'leo']
+
+# print("Here are the first three players on my team:")
+# for player in players[:3]:
+#     print(player.title())
+
+
+# Copying a List 
+
+# my_foods = ['pizza', 'falafel', 'carrot cake']
+# friend_foods = my_foods[:]
+
+# print("My favorite foods are:")
+# print(my_foods)
+
+# print("\nMy friend's favorite food are:")
+# print(friend_foods)
+
+
+# my_foods = ['pizza', 'falafel', 'carrot cake']
+# friend_foods = my_foods[:]
+
+# my_foods.append('cannoli')
+# friend_foods.append('ice cream')
+
+# print("My favorite foods are:")
+# print(my_foods)
+
+# print("\nMy friend's favorite food are:")
+# print(friend_foods)
+
+
+
+# my_foods = ['pizza', 'falafel', 'carrot cake']
+
+# # This does not work!
+# friend_foods = my_foods
+
+# my_foods.append('cannoli')
+# friend_foods.append('ice cream')
+
+# print("My favorite foods are:")
+# print(my_foods)
+
+# print("\nMy friend's favorite food are:")
+# print(friend_foods)
+
+
+# -----------------------------------------------------------------------------
+
+
+# Exercies
+
+# 4.10 slices 
+
+cubes = [cube**3 for cube in range(1,11)]
+# for cube in cubes:
+#     print(cube)
+
+# print("Here is the first three values of the list:")
+# for cube in cubes[:3]:
+#     print(cube)
+
+# print("\nHere is the three values from the middle of the list:")
+# for cube in cubes[3:6]:
+#     print(cube)
+
+# print("\nHere is the last three values from the list:")
+# for cube in cubes[-3:]:
+#     print(cube)
+
+
+# 4.11 My Pizzas, Your Pizzas
+
+# my_pizzas = ['new york style pizza', 'chicago pizza', 'sicilian pizza']
+# your_pizzas = my_pizzas[:]
+
+# my_pizzas.append('myanmar pizza')
+# your_pizzas.append('england pizza')
+
+# print("My fvaorite pizzas are:")
+# for pizza in my_pizzas:
+#     print(pizza.title())
+
+# print("\nYour favorite pizzas are:")
+# for pizza in your_pizzas:
+#     print(pizza.title())
+
+# 4.12 More Loops 
+
+# -----------------------------------------------------------------------------
+
+
