c2c76a762b578dc2d7f59581f71e16aca82bf679
NEO <nainglinaung100kta@gmail.com>
2025-04-14 20:07:08 +0630

    modified Mon Apr 14 8:07 PM 2025

--- python_basics/if_statements.py
+++ python_basics/if_statements.py
@@ -244,3 +244,218 @@ user = 'marie'
 #     print("You are not the wantend person!")
 # else:
 #     print("Come with me!")
+
+
+# -----------------------------------------------------------------------------
+
+# If Statement
+
+# Simple if Statement
+
+# age = 19
+# if age >= 18:
+#     print("You're old enough to vote!")
+#     print("Have you registered to vote yet?")
+
+
+# if-else Statement
+
+# age = 17
+# if age >= 18:
+#     print("You are old enough to vote!")
+#     print("Have you registered to vote yet?")
+# else:
+#     print("Sorry, you are too young to vote.")
+#     print("Please register to vote as soon as you turn 18!")
+
+
+# if-elif-else statement
+
+# age = 12
+# if age < 4:
+#     print("Your admission cost is 0$.")
+# elif age < 18:
+#     print("Your admission cost is 25$.")
+# else:
+#     print("Your admission cost is 40$.")
+
+
+# age = 12
+# if age < 4:
+#     price = 0
+# elif age < 18:
+#     price = 25
+# else:
+#     price = 40
+
+# print(f"Your admission cost is {price}$.")
+
+
+# Using Multiple elif Blocks
+
+# age = 12
+# if age < 4:
+#     price = 0
+# elif age < 18:
+#     price = 25
+# elif age < 65:
+#     price = 40
+# else:
+#     price = 20
+
+# print(f"Your admission cost is {price}$.")
+
+
+# Omitting the else Block 
+
+# age = 12
+
+# if age < 4:
+#     price = 0 
+# elif age < 18:
+#     price = 25
+# elif age < 65:
+#     price = 40
+# elif age >= 65:
+#     price = 20 
+
+# print(f"Your admission cost is {price}$.")
+
+
+# Testing Multiple Conditions
+
+# requested_toppings = ['mushrooms', 'extra cheese']
+
+# if 'mushrooms' in requested_toppings:
+#     print("Adding mushrooms!")
+# if 'pepperoni' in requested_toppings:
+#     print("Adding pepperoni!")
+# if 'extra cheese' in requested_toppings:
+#     print("Adding extra cheese!")
+
+# print("\nFinished Making Your Pizza!")
+
+# -----------------------------------------------------------------------------
+
+# Exercises
+
+# 5.3 Alien Colors #1
+
+# alien_color = 'green'
+
+# if alien_color == 'green':
+#     point = 5
+
+# print(f"You just earned {point} points!")
+# # pass 
+
+# alien_color = 'yellow'
+
+# if alien_color == 'green':
+#     point = 5
+
+# print(f"You just earned {point} points!")
+# # fail
+
+
+# 5.4 Alien Colors #2
+
+# alien_color = 'green'
+
+# if alien_color == 'green':
+#     point = 5
+# else:
+#     point = 10
+
+# print(f"You just earned {point} points!")
+
+# # run if block 
+
+# alien_color = 'yellow'
+
+# if alien_color == 'green':
+#     point = 5
+# else:
+#     point = 10
+
+# print(f"You just earned {point} points!")
+
+# #run else block 
+
+
+# 5.5 Alien Colors #3
+
+# alien_color = 'green'
+
+# if alien_color == 'green':
+#     point = 5
+# elif alien_color == 'yellow':
+#     point = 10
+# elif alien_color == 'red':
+#     point = 15
+
+# print(f"You just earned {point} points!")
+# # green 
+
+# alien_color = 'yellow'
+
+# if alien_color == 'green':
+#     point = 5
+# elif alien_color == 'yellow':
+#     point = 10
+# elif alien_color == 'red':
+#     point = 15
+
+# print(f"You just earned {point} points!")
+# # yellow 
+
+# alien_color = 'red'
+
+# if alien_color == 'green':
+#     point = 5
+# elif alien_color == 'yellow':
+#     point = 10
+# elif alien_color == 'red':
+#     point = 15
+
+# print(f"You just earned {point} points!")
+# # red 
+
+
+# 5.6 Stages of Life 
+
+# age = 50
+
+# if age < 2:
+#     stage = 'baby'
+# elif age < 4:
+#     stage = 'toddler'
+# elif age < 13:
+#     stage = 'kid'
+# elif age < 20:
+#     stage = 'teenager'
+# elif age < 65:
+#     stage = 'adult'
+# elif age >= 65:
+#     stage = 'elder'
+
+# if stage == 'adult' or stage == 'elder':
+#     print(f"You are an {stage}!")
+# else:
+#     print(f"You are a {stage}!")
+
+
+# 5.7 Favorite Fruit
+
+# fav_fruits = ['banana', 'grape', 'apple']
+
+# if 'banana' in fav_fruits:
+#     print("You really like bananas!")
+# if 'orange' in fav_fruits:
+#     print("You really like oranges!")
+# if 'apple' in fav_fruits:
+#     print("You really like apples!")
+# if 'grape' in fav_fruits:
+#     print("You really like grapes!")
+# if 'lime' in fav_fruits:
+#     print("You really like limes!")
